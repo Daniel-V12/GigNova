240ba1b308e5c7e455d5f1f0c2e9a7c7d76d9ada
USER <USER@DESKTOP-CS9D2NS>
2025-12-05 12:23:14 +0200

    fixed catalog function and added seller controller

---
+++ GigNovaWS/Controllers/SellerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GigNovaWS.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SellerController : ControllerBase
+    {
+        RepositoryUOW repositoryUOW;
+        public SellerController()
+        {
+            this.repositoryUOW = new RepositoryUOW();
+        }
+    }
+
+   
+}
