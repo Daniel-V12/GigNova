77007ffeaccce3305962fb92270776657e782302
Daniel-V12 <danielwolf13042008@gmail.com>
2026-04-29 00:27:55 +0300

    started working on wpf, added dashboard for main window, homepage and catalog page (but catalog page is static and bugged for now) made the color theme red and black instead of blue and black

--- GigNovaWPFApp/MainWindow.xaml.cs
+++ GigNovaWPFApp/MainWindow.xaml.cs
@@ -1,25 +1,24 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using GigNovaWPFApp.UserControls;
 
 namespace GigNovaWPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            
             InitializeComponent();
+            MainFrame.Content = new HomePage();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new HomePage();
+        }
+
+        private void ViewCatalogButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new CatalogPage();
         }
     }
 }
